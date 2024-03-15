2cb182b9b0b9e2517e5520dcf65e195274d56185
ugniusalekna <ugnius.alekna@gmail.com>
2024-03-15 21:30:17 +0200

    added different data objects

---
+++ data.py
@@ -0,0 +1,36 @@
+import numpy as np
+import torch
+
+class Point:
+    def __init__(self, value, device, grad=True):
+        self.value = value
+        self.device = device
+        self.grad = grad
+        self.tensor = torch.tensor([value], requires_grad=grad).to(device)
+        
+    def __repr__(self):
+        return f'Value({self.value}; device={self.device}; grad={self.grad})'
+    
+class Interval:
+    def __init__(self, start, end, steps):
+        self.start = start
+        self.end = end
+        self.steps = steps
+        self._linspace = torch.linspace(start, end, steps).unsqueeze(1)
+        
+    @property # to call w/o ()
+    def linspace(self):
+        return self._linspace.requires_grad_(True)
+    
+    def __repr__(self):
+        return f'Interval({self.start}, {self.end}, steps={self.steps})'
+        
+    
+class Data:
+    def __init__(self, domain, ode, ics, solution=None):
+        self.domain = domain.linspace
+        self.ode = ode
+        self.ics = ics
+        self.solution = solution(self.domain) if solution is not None else None
+        self.solution_fn = solution
+        
