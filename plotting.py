dbc8dcf4e99ac3028a8be85b23649a2b5648b351
ugniusalekna <ugnius.alekna@gmail.com>
2024-03-15 21:32:28 +0200

    concluded solution, phase plots to ODEPlotter class

--- plotting.py
+++ plotting.py
@@ -1,9 +1,122 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import imageio
 
 
+class ODEPlotter:
+    def __init__(self, solver):
+        
+        self.solver = solver
+        self.model = solver.model
+        self.domain = self._get_domain()
+        self.output = self._get_output()
+        self.derivatives = self._get_derivatives()
+        self.solution = self._get_solution()
+        
+    def _get_domain(self, torch=False):
+        return self.solver.domain.detach().cpu().numpy() if not torch else self.solver.domain.detach().cpu()
+    
+    def _get_output(self):
+        domain = self.solver.domain
+        output = self.model(domain)
+        return output.detach().cpu().numpy()
+    
+    def _get_derivatives(self):
+        domain = self.solver.domain
+        output = self.model(domain)
+        derivatives = self.solver._compute_gradients(domain, output)
+        return [d.detach().cpu().numpy() for d in derivatives]
+
+    def _get_solution(self):
+        return self.solver.solution.detach().cpu().numpy() if self.solver.solution is not None else None
+        
+    def losses(self, losses_domain, losses_ic):
+        plt.figure(figsize=(10, 6))
+        plt.plot(losses_domain, label='Domain Loss', color='blue')
+        plt.plot(losses_ic, label='Initial Condition Loss', color='red')
+        plt.title('Losses During Training')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.yscale('log')
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+        
+    def numerical_solution(self):
+        plt.figure(figsize=(10, 6))
+        plt.plot(self.domain, self.output, label='Approximated', color='red')
+        if self.solution is not None:
+            plt.plot(self.domain, self.solution, label='Exact', color='blue')
+        plt.xlabel('$t$')
+        plt.ylabel('$y(t)$')
+        plt.grid(True)
+        plt.legend()
+        plt.title('Solution vs time')
+        plt.show()
+        
+    def phase_space(self, exact_derivatives=None):
+        ode_order = self.solver.ode_order
+        if ode_order == 2:
+            self._plot_2d_phase_space(exact_derivatives)
+        elif ode_order == 3:
+            self._plot_3d_phase_space(exact_derivatives)
+        else:
+            raise ValueError('Can only plot phase space for ODEs of order 2 and 3')
+        
+    def extended_phase_space(self, exact_derivatives=None):
+        ode_order = self.solver.ode_order
+        if ode_order == 2:
+            self._plot_3d_extended_phase_space(exact_derivatives)
+        else:
+            raise ValueError('Can only plot phase space for ODEs of order 2')
+        
+    def _plot_2d_phase_space(self, exact_derivatives):
+        plt.figure(figsize=(10, 6))
+        plt.plot(self.output, self.derivatives[0], label='Approximated', color='red')
+        if exact_derivatives is not None and self.solution is not None:
+            exact_dy = exact_derivatives[0](self._get_domain(torch=True)).detach().cpu().numpy()
+            plt.plot(self.solution, exact_dy, label='Exact', color='blue')
+        plt.xlabel('$y(t)$')
+        plt.ylabel('$y\'(t)$')
+        plt.grid(True)
+        plt.legend()
+        plt.title('Phase space')
+        plt.show()
+        
+    def _plot_3d_phase_space(self, exact_derivatives):
+        fig = plt.figure(figsize=(16, 9))
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(self.output, self.derivatives[0], self.derivatives[1], label='Approximated', color='red')
+        if exact_derivatives is not None and self.solution is not None:
+            exact_dy = exact_derivatives[0](self._get_domain(torch=True)).detach().cpu().numpy()
+            exact_dy2 = exact_derivatives[1](self._get_domain(torch=True)).detach().cpu().numpy()
+            ax.plot(self.solution, exact_dy, exact_dy2, label='Exact', color='blue')
+        ax.set_xlabel('$y(t)$')
+        ax.set_ylabel('$y\'(t)$')
+        ax.set_zlabel('$y\'\'(t)$')
+        plt.grid(True)
+        plt.legend()
+        plt.title('Phase space')
+        plt.show()
+        
+    def _plot_3d_extended_phase_space(self, exact_derivatives):
+        fig = plt.figure(figsize=(16, 9))
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(self.domain, self.output, self.derivatives[0], label='Approximated', color='red')
+        if exact_derivatives is not None and self.solution is not None:
+            exact_dy = exact_derivatives[0](self._get_domain(torch=True)).detach().cpu().numpy()
+            ax.plot(self.domain, self.solution, exact_dy, label='Exact', color='blue')
+        ax.set_xlabel('$t$')
+        ax.set_ylabel('$y(t)$')
+        ax.set_zlabel('$y\'(t)$')
+        plt.grid(True)
+        plt.legend()
+        plt.title('Extended phase space')
+        plt.show()
+    
+    
 def write_gif(save_path, images):
     
     if not os.path.exists(save_path.split("/")[0]):
@@ -39,8 +152,8 @@ def plot_figure(domain, outputs, solution=None, model=None, epoch=None, loss=Non
     else:
         plt.ylim([outputs_np.min()-1, 1.5*outputs_np.max()])
     
-    last_layer_weights = model.blocks[-1].layers[-1].weight.data.tolist()[0]
-    last_layer_bias = model.blocks[-1].layers[-1].bias.data.item()
+    last_layer_weights = model.blocks[-1].layers[-1].weight.data.tolist()[0] if model is not None else None
+    last_layer_bias = model.blocks[-1].layers[-1].bias.data.item() if model is not None else None
     
     if len(last_layer_weights) <= 6:
         weights_text = 'Weights of Last Layer: ' + ', '.join([f'{w:.4f}' for w in last_layer_weights])
@@ -59,16 +172,3 @@ def plot_figure(domain, outputs, solution=None, model=None, epoch=None, loss=Non
     
     plt.savefig(save_path)
     plt.close()
-    
-    
-def plot_losses(losses_domain, losses_ic):
-    plt.figure(figsize=(10, 6))
-    plt.plot(losses_domain, label='Domain Loss', color='blue')
-    plt.plot(losses_ic, label='Initial Condition Loss', color='red')
-    plt.title('Losses During Training')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.yscale('log')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
