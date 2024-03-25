a83fe1d8c3863a5867eae251a7222edd2aa67e13
ugniusalekna <ugnius.alekna@gmail.com>
2024-03-26 01:04:51 +0200

    made ODESolver suitable for ODEs and systems of ODEs

--- solver.py
+++ solver.py
@@ -3,7 +3,6 @@ import shutil
 import inspect
 import torch
 import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR
 from tqdm import tqdm
 from abc import ABC, abstractmethod
 from collections import defaultdict
@@ -18,34 +17,34 @@ class BaseSolver:
         self.domain = data.domain
         self.device = device
         
-    def compile(self, optimizer, lr=1e-3, **kwargs):
-
+    def compile(self, optimizer, scheduler=None, lr=1e-3, **kwargs):
         optimizer_kwargs = kwargs.get('opt', {})
         scheduler_kwargs = kwargs.get('sch', {})
-
         self.optimizer = self._configure_optimizer(optimizer, lr, **optimizer_kwargs)
-        self.scheduler = self._configure_scheduler(**scheduler_kwargs)
+        self.scheduler = self._configure_scheduler(scheduler, **scheduler_kwargs)
         self._remove_temp()
             
     def _remove_temp(self):
         if os.path.exists('temp'):
             shutil.rmtree('temp')
             
-    def _configure_optimizer(self, optimizer, lr, **kwargs):
-        optimizers = {
+    def _configure_optimizer(self, optimizer, lr=1e-3, **kwargs):
+        return {
             'adam': optim.Adam(self.model.parameters(), lr=lr, **kwargs),
             'adamW': optim.AdamW(self.model.parameters(), lr=lr, **kwargs),
             'adagrad': optim.Adagrad(self.model.parameters(), lr=lr, **kwargs),
             'SGD': optim.SGD(self.model.parameters(), lr=lr, **kwargs),
-            'LBFGS': optim.LBFGS(self.model.parameters(), lr=lr, **kwargs)
-        }
-        return optimizers.get(optimizer, None)
+            'LBFGS': optim.LBFGS(self.model.parameters(), lr=lr, **kwargs),
+        }.get(optimizer, None)
      
-    def _configure_scheduler(self, **kwargs):
-        if 'step_size' in kwargs and 'gamma' in kwargs:
-            return StepLR(self.optimizer, **kwargs)
-        else:
-            return StepLR(self.optimizer, step_size=10, gamma=1.0)
+    def _configure_scheduler(self, scheduler, **kwargs):
+        match scheduler:
+            case 'step':
+                return optim.lr_scheduler.StepLR(self.optimizer, **kwargs)
+            case 'onecycle':
+                return optim.lr_scheduler.OneCycleLR(self.optimizer, **kwargs)
+            case _:
+                return optim.lr_scheduler.StepLR(self.optimizer, step_size=100, gamma=0.95)
 
     def _get_norm_func(self, norm):
         loss_fn = {
@@ -87,8 +86,7 @@ class BaseSolver:
 
         return loss_records
             
-    def create_gif(self, gif_save_path):
-        
+    def create_gif(self, gif_save_path):   
         gif_save_path = f"{gif_save_path}/{self.model.activation}_{'_'.join(map(str, self.model.hidden_layers))}"
         
         def sort_key(filename):
@@ -107,7 +105,7 @@ class BaseSolver:
 class FunctionApproximator(BaseSolver):
     def __init__(self, model, data, device=torch.device('cpu')):
         super().__init__(model, data, device)
-        self.solution = data.solution_fn.evaluate(self.domain)
+        self.solution = data.solution.evaluate(self.domain)
     
     def compute_loss(self, domain, outputs, loss_records):
         loss_fn = self._get_norm_func(norm='L2')
@@ -123,21 +121,24 @@ class FunctionApproximator(BaseSolver):
         
         results = f"Model parameter count: {parameter_count}\n--- At t = {value.item():.4f} ---\n"
                 
-        exact_value = self.data.solution_fn.evaluate(value)
+        exact_value = self.data.solution.evaluate(value)
         value_error = abs(output.item() - exact_value.item())
         results += f"Solution | Computed: {output.item():.4f}, Exact: {exact_value.item():.4f}, Abs Error: {value_error:.4f}\n"
 
         print(results)
-        
-    
-class BaseODEs(BaseSolver):
+
+
+class ODESolver(BaseSolver):
     def __init__(self, model, data, device=torch.device('cpu')):
         super().__init__(model, data, device)
+        self.odes = data.equation
         self.ics = data.ics
+        self.solution = data.solution.evaluate(self.domain)
+        self.ode_order = len(inspect.signature(self.odes.f).parameters) - 2
         self.n_equations = model.output_features
 
-    def compile(self, optimizer, lr=1e-3, loss_weights=[1.0, 1.0], **kwargs):
-        super().compile(optimizer, lr, **kwargs)
+    def compile(self, optimizer, scheduler, lr=1e-3, loss_weights=[1.0, 1.0], **kwargs):
+        super().compile(optimizer, scheduler, lr, **kwargs)
         self.lambda_domain, self.lambda_ic = loss_weights     
 
     def compute_loss(self, domain, outputs, loss_records):
@@ -149,107 +150,78 @@ class BaseODEs(BaseSolver):
         return loss
 
     def _compute_domain_loss(self, domain, outputs, norm):
-        gradients = self._compute_gradients(domain, outputs)
+        gradients = self.compute_derivatives(domain, outputs)
         loss_fn = self._get_norm_func(norm)
         odes = self.odes.evaluate(domain, outputs, *gradients)
-        zeros = torch.zeros_like(odes, dtype=torch.float32, device=self.device, requires_grad=True)
+        zeros = torch.zeros_like(odes, requires_grad=True, dtype=torch.float32, device=self.device)
         loss_domain = loss_fn(odes, zeros)
         return loss_domain
-    
+
     def _compute_ic_loss(self, ics, norm):
-        x_0 = torch.tensor([ics[0]], requires_grad=True, device=self.device, dtype=torch.float32)
-        output = self.model(x_0)
-        predicted_ics = self._get_predicted_ics(x_0, output)
-        true_ics = torch.stack([torch.tensor([value], device=self.device, dtype=torch.float32) for value in ics[1:]])
+        x_0 = torch.tensor([[ics[0][0]]], requires_grad=True, dtype=torch.float32, device=self.device)
+        y_0 = self.model(x_0)
+        predicted_ics = self._get_predicted_ics(x_0, y_0)
+        true_ics = self._get_true_ics(ics)
         loss_fn = self._get_norm_func(norm)
         ic_loss = loss_fn(predicted_ics, true_ics)
         return ic_loss
 
-
-class ODESolver(BaseODEs):
-    def __init__(self, model, data, device=torch.device('cpu')):
-        super().__init__(model, data, device)
-        self.odes = data.equation_fn
-        self.solution = data.solution_fn.evaluate(self.domain)
-        self.ode_order = len(inspect.signature(self.odes.function).parameters) - 2
-    
-    def _compute_gradients(self, domain, model_output):
-        gradients = [model_output]
-        for _ in range(self.ode_order):
-            grad = torch.autograd.grad(
-                outputs=gradients[-1], inputs=domain,
-                grad_outputs=torch.ones_like(gradients[-1]).to(self.device),
-                create_graph=True,
-            )[0]
-            gradients.append(grad)
-        return gradients[1:]
-
-    def _get_predicted_ics(self, x_0, output):
-        return torch.stack([output] + self._compute_gradients(x_0, output)[:-1])
-
-    def evaluate(self, value, exact_derivatives=None):
-        self.model.eval()
-        parameter_count = self.model.param_count
-        value = torch.tensor([value], requires_grad=True).to(self.device)
-        output = self.model(value)
-        computed_derivatives = self._compute_gradients(value, output)
-        
-        results = f"Model parameter count: {parameter_count}\n--- At t = {value.item():.4f} ---\n"
-        if self.data.solution_fn is not None:
-            exact_value = self.data.solution_fn.evaluate(value).item()
-            value_error = abs(output.item() - exact_value)
-            results += f"Solution | Computed: {output.item():.4f}, Exact: {exact_value:.4f}, Abs Error: {value_error:.4f}\n"
-        else:
-            results += f"Solution | Computed: {output.item():.4f}\n"
-        if exact_derivatives is not None:
-            for i, (computed_derivative, exact_derivative_fn) in enumerate(zip(computed_derivatives, exact_derivatives)):
-                exact_derivative = exact_derivative_fn(value)
-                derivative_error = abs(computed_derivative.item() - exact_derivative.item())
-                results += f"Derivative order {i+1} | Computed: {computed_derivative.item():.4f}, Exact: {exact_derivative.item():.4f}, Abs Error: {derivative_error:.4f}\n"
-        else:
-            for i, computed_derivative in enumerate(computed_derivatives):
-                results += f"Derivative order {i+1} | Computed: {computed_derivative.item():.4f}\n"
-        print(results)
-        
-        
-class SysODESolver(BaseODEs):
-    def __init__(self, model, data, device=torch.device('cpu')):
-        super().__init__(model, data, device)
-        self.odes = data.equation_sys
-        self.solution = data.solution_sys.evaluate(self.domain)
-        self.ode_order = len(inspect.signature(self.odes.system).parameters) - 2
-    
-    def _compute_gradients(self, domain, model_output):
-        gradients = [model_output]
-        for _ in range(self.ode_order):
-            grads = []
-            for j in range(self.n_equations):
-                grad = torch.autograd.grad(
-                    outputs=gradients[-1][:, j], inputs=domain,
-                    grad_outputs=torch.ones_like(gradients[-1][:, j]).to(self.device),
+    def derivative(self, y, x):
+        dydx = torch.autograd.grad(
+                    outputs=y, inputs=x,
+                    grad_outputs=torch.ones_like(y).to(self.device),
                     create_graph=True,
                 )[0]
-                grads.append(grad)
-            grads = torch.stack(grads, dim=1).squeeze(-1)
-            gradients.append(grads)
-        return gradients[1:]
+        return dydx
+
+    def compute_derivatives(self, domain, model_output):
+        derivatives = [model_output]
+        for _ in range(self.ode_order):
+            grad = []
+            for j in range(self.n_equations):
+                grad.append(self.derivative(derivatives[-1][:, j], domain))
+            derivatives.append(torch.hstack(grad))
+        return derivatives[1:]
     
-    def _get_predicted_ics(self, x_0, output):
-        return torch.stack([output], dim=-1)
-    
-    def evaluate(self, value):
-        self.model.eval()
-        parameter_count = self.model.param_count
-        value = torch.tensor([value], requires_grad=True).to(self.device)
-        outputs = self.model(value)
+    def _get_true_ics(self, ics):
+        ic_matrix = torch.zeros((self.n_equations, self.ode_order), dtype=torch.float32, device=self.device)
+        for eq_idx, values in enumerate(ics):
+            ic_matrix[eq_idx, :] = torch.tensor(values[1:], dtype=torch.float32, device=self.device)
+        return ic_matrix
+            
+    def _get_predicted_ics(self, x_0, y_0):
+        derivatives = self.compute_derivatives(x_0, y_0)[:-1]
+        ic_matrix = torch.zeros((self.n_equations, self.ode_order), dtype=torch.float32, device=self.device)
+        ic_matrix[:, 0] = y_0
+        for eq_idx, derivative in enumerate(derivatives):
+            ic_matrix[:, eq_idx + 1] = derivative
+        return ic_matrix
+
+    def evaluate(self, value, exact_derivatives=None):
         
-        results = f"Model parameter count: {parameter_count}\n--- At t = {value.item():.4f} ---\n"
-        for i in range(self.n_equations):
-            output = outputs[i]
-            if self.data.solution_sys is not None:
-                exact_value = self.data.solution_sys.evaluate(value).squeeze(0)[i]
-                value_error = abs(output.item() - exact_value.item())
-                results += f"Solution {i+1} | Computed: {output.item():.4f}, Exact: {exact_value:.4f}, Abs Error: {value_error:.4f}\n"
+        self.model.eval()
+        value = torch.tensor([[value]], requires_grad=True, dtype=torch.float32).to(self.device)
+        outputs = self.model(value)
+        outputs = outputs.unsqueeze(0) if outputs.dim() == 1 else outputs
+        computed_derivatives = self.compute_derivatives(value, outputs)
+        
+        results = f"Model parameter count: {self.model.param_count}\n--- At t = {value.item():.4f} ---\n"
+        for eq_index in range(outputs.shape[1]):
+            output = outputs[:, eq_index]
+
+            if self.solution is not None:
+                exact_value = self.data.solution.evaluate(value)[:, eq_index]
+                value_error = abs(output - exact_value)
+                results += f"Equation {eq_index + 1} | Solution: Computed: {output.item():.4f}, Exact: {exact_value.item():.4f}, Abs Error: {value_error.item():.4f}\n"
             else:
-                results += f"Solution {i+1} | Computed: {output.item():.4f}\n"
+                results += f"Equation {eq_index + 1} | Solution: Computed: {output.item():.4f}\n"
+            
+            # if exact_derivatives is not None:
+            #     exact_d_value = exact_derivatives.evaluate(value)[:, eq_index]
+            #     for order, (comp_ders, exact_ders) in enumerate(zip(computed_derivatives, exact_derivatives)):
+            #         exact_der = exact_ders(value)[:, eq_index]
+            #         comp_der = comp_ders[:, eq_index]
+            #         derivative_error = abs(comp_der - exact_der)
+            #         results += f"Equation {eq_index + 1}, Derivative order {order + 1} | Computed: {comp_der.item():.4f}, Exact: {exact_der.item():.4f}, Abs Error: {derivative_error.item():.4f}\n"
+        
         print(results)
